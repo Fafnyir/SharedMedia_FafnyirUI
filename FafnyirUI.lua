7f05a1161a36ef820fe67a1f52b6c5af294e4662
fpatten <34715459+fpatten@users.noreply.github.com>
2022-08-21 15:55:57 -0400

    1.0

---
+++ FafnyirUI.lua
@@ -0,0 +1,26 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+
+-- -----
+--   FONT
+-- -----
+LSM:Register ("font", "-FafnyirUI", [[Interface\Addons\SharedMedia_FafnyirUI\fonts\Urbanist-SemiBold.ttf]])
+LSM:Register ("font", "-FafnyirUI_Bold", [[Interface\Addons\SharedMedia_FafnyirUI\fonts\Urbanist-Bold.ttf]])
+LSM:Register ("font", "-Fafnyir_Black", [[Interface\Addons\SharedMedia_FafnyirUI\fonts\Urbanist-Black.ttf]])
+LSM:Register ("font", "- BOLTZZ", [[Interface\Addons\SharedMedia_FafnyirUI\fonts\BOLTZZ.ttf]])
+--   SOUND
+-- -----
+
+-- -----
+--   STATUSBAR
+-- -----
+LSM:Register("statusbar", "- FafnyirUI Clean", [[Interface\Addons\SharedMedia_FafnyirUI\statusbar\Clean.tga]])
