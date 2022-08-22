c603246636595a3fb55c1a4fac5d88d86e9d9e91
fpatten <34715459+fpatten@users.noreply.github.com>
2022-08-21 22:12:12 -0400

    Fonts
    Fixed Font names

--- FafnyirUI.lua
+++ FafnyirUI.lua
@@ -15,7 +15,7 @@ local LSM = LibStub("LibSharedMedia-3.0")
 -- -----
 LSM:Register ("font", "-FafnyirUI", [[Interface\Addons\SharedMedia_FafnyirUI\fonts\Urbanist-SemiBold.ttf]])
 LSM:Register ("font", "-FafnyirUI_Bold", [[Interface\Addons\SharedMedia_FafnyirUI\fonts\Urbanist-Bold.ttf]])
-LSM:Register ("font", "-Fafnyir_Black", [[Interface\Addons\SharedMedia_FafnyirUI\fonts\Urbanist-Black.ttf]])
+LSM:Register ("font", "-FafnyirUI_Black", [[Interface\Addons\SharedMedia_FafnyirUI\fonts\Urbanist-Black.ttf]])
 LSM:Register ("font", "- BOLTZZ", [[Interface\Addons\SharedMedia_FafnyirUI\fonts\BOLTZZ.ttf]])
 --   SOUND
 -- -----
